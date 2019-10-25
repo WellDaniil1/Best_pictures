18faba7c4b53bb3edf5cca80d00988b2087c5298
AlenaIraTanya <alennedvic@gmail.com>
2019-10-25 11:38:08 +0700

    Add tasks

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Case 1
 # Task 1
 # TO DO(Danila): Triangle figure
-def triangle(x,y,a,b,c,color,angle)
+def triangle(x,y,a,color,angle)
 pass
 # TO DO(Daniil): Square figure
 def square(x,y,a,color,angle)
