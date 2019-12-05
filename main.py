9366023bd934910aa5353fcc04ffedc8490e809e
A335_ik <danila.zykov@gmail.com>
2019-12-05 17:55:11 +0700

    Created Galstuk

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 # Task 1
 
 import turtle
+
 turtle.hideturtle()
+
 def triangle(a,b,x,y,anglemain,angle,angle1,color):
     turtle.up()
     turtle.goto(a,b)
@@ -30,6 +32,7 @@ def square(x,y,a,angle,color):
         turtle.left(90)
         turtle.forward(a)
     turtle.end_fill()
+    turtle.goto(x,y)
     turtle.left(180 - angle)
 
 def rhombus(a,b,x,anglemain,angle,color):
@@ -40,11 +43,13 @@ def rhombus(a,b,x,anglemain,angle,color):
     turtle.begin_fill()
     turtle.left(anglemain)
     for i in range(2):
+        turtle.forward(x)
         turtle.left(angle)
         turtle.forward(x)
         turtle.left(180 - angle)
-        turtle.forward(x)
     turtle.end_fill()
+    turtle.goto(a,b)
+    turtle.right(anglemain)
 
 triangle(0,300,50,50,30,60,60,'orange')
 triangle(0,300,50,50,90,60,60,'orange')
@@ -53,3 +58,8 @@ triangle(0,300,50,50,150,60,60,'orange')
 triangle(0,300,50,50,-30,60,60,'orange')
 triangle(0,300,50,50,-90,60,60,'orange')
 triangle(0,300,50,50,-150,60,60,'orange')
+
+square(-300,300,30,0,'blue')
+square(-270,330,30,0,'blue')
+rhombus(-285,300,21,45,90,'yellow')
+rhombus(-285,300,58,255,30,'yellow')
